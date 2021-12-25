9d05aeeda45d89cffc5b9f19fd7cff7ca5150dfa
Yoav <yoav.meron11@gmail.com>
2021-12-25 09:52:05 +0200

    Changed what is returned in getAllFinishedRankings function.

--- src/main/java/org/wikiRelationsAlgorithm/core/RankingThreadList.java
+++ src/main/java/org/wikiRelationsAlgorithm/core/RankingThreadList.java
@@ -9,7 +9,7 @@ public class RankingThreadList {
     public void addRankingThread(LinksObject linksObject) {
         RankingThread rankingThread = new RankingThread(linksObject);
         this.rankingThreads.add(rankingThread);
-        rankingThread.start();
+        new Thread(rankingThread).start();
     }
 
 
@@ -20,14 +20,10 @@ public class RankingThreadList {
         for (RankingThread rankingThread: rankingThreads) {
             if (rankingThread.isFinished()) {
                 finishedThreads.add(rankingThread);
-                Hashtable<String, ArrayList<Hashtable<String,String>>> links = new Hashtable<>();
+                Hashtable<String, Object> links = new Hashtable<>();
                 links.put("Links", rankingThread.getRankedTitleToLink());
-                Hashtable<String, Integer> idTable = new Hashtable<>();
-                idTable.put("id", rankingThread.linksObject.getId());
-                Hashtable<String, String> original = new Hashtable<>();
-                original.put("Original Link", rankingThread.linksObject.getOriginalLink());
-                finished.add(idTable);
-                finished.add(original);
+                links.put("id", rankingThread.linksObject.getId());
+                links.put("Original Link", rankingThread.linksObject.getOriginalLink());
                 finished.add(links);
 
             }
