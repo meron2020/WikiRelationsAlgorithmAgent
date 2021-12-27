f083646a675703db9af5d509d201389f0266d266
Yoav <yoav.meron11@gmail.com>
2021-12-27 18:07:05 +0200

    Created HeadMasterCommsResource

---
+++ src/main/java/org/wikiRelationsAlgorithm/resources/HeadMasterCommsResource.java
@@ -0,0 +1,26 @@
+package org.wikiRelationsAlgorithm.resources;
+
+import org.wikiRelationsAlgorithm.core.RankingThreadList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/communication")
+public class HeadMasterCommsResource {
+    RankingThreadList rankingThreadList;
+
+    public HeadMasterCommsResource(RankingThreadList rankingThreadList) {
+        this.rankingThreadList = rankingThreadList;
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Integer getFinishedRankings() {
+        return this.rankingThreadList.rankingThreadsListSize();
+    }
+
+}
